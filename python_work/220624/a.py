680457760d046e53e50129ddb889cb6282d97345
nowgyung <vetibeti@naver.com>
2022-06-24 13:57:57 +0900

    20624

---
+++ python_work/220624/a.py
@@ -0,0 +1,12 @@
+def greet():
+        print("안녕하세요")
+        print("파이썬의 세계로 오신것을 환영합니다.")
+        a= 10
+        b= 20
+        print(a)
+        print(b)
+
+
+greet()
+print("aa")
+greet()
---
+++ python_work/220624/b.py
@@ -0,0 +1,7 @@
+def aaa(name, age):
+    print(name, 'hello')
+    print(age)
+
+
+aaa('john', 30)
+aaa('kelly', 32)
