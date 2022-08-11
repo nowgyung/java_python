0a4fe5966fc9626457cd2b8f5c183c1128c0190e
nowgyung <vetibeti@naver.com>
2022-08-11 17:44:29 +0900

    220811

--- flask_work/ex05.py
+++ flask_work/ex05.py
@@ -1,10 +1,13 @@
-from turtle import down
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 
 plt.style.use("seaborn")
+matplotlib.rcParams['font.family']='Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus']=False
+
 data = pd.read_excel('./static/data/carprice.xlsx')
 dt = data.groupby('년식').mean()
 print(dt)
@@ -22,12 +25,11 @@ plt.bar(dt.index.to_numpy(),bae,width=0.3, alpha=0.8, label='bae', bottom=joong+
 # plt.bar(dt.index.to_numpy(),dt['가격'].to_numpy(),width=0.1, alpha=0.5, label = 'cost')
 # plt.bar(dt.index.to_numpy()+0.1,dt['배기량'].to_numpy(),width=0.1, alpha=0.5, label = 'volume')
 
-plt.xticks(rotation=30)
-plt.yticks(rotation=30)
+plt.title('년식별 중량,가격,배기량')
 plt.legend()
 plt.grid()
 # plt.tight_layout()
 # plt.show()
 
-plt.savefig('joong_ka_bae.png')
+plt.savefig('./static/car/joong_ka_bae.png')
 
